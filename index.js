61bb8991f08a31c4682b2e7b1e98c1c8193d63f5
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-03-05 21:04:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,3 +20,9 @@ function appendKitten(name){
 function prependKitten(name){
   return [...name, kittens]
 }
+function removeLastKitten(){
+  return kittens.slice(0,-1)
+}
+function removeFirstKitten(){
+  return kittens.slice(1,-1)
+}
