39d00819e8947a393b11f20d308da16e325b2e8b
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-03-05 20:48:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const app = "I don't do much."
+function destructivelyAppendKitten(n){
+  kittens.push(n)
+  return kittens
+}
+function destructivelyPrependKitten(n){
+  kittens.unshift(n)
+  return kittens
+}
+function destructivelyRemoveLastKitten(){
+  return kittens.shift(0,-1)
+}
